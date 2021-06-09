4d4edec2e3c42e151aa72740976a638d668214dc
oracle <tmfdl7586@naver.com>
2021-06-09 11:38:16 +0900

    C Sharp Restart

---
+++ Basic060921/Basic060921/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic060921
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("새로운 main");
+
+            // 1차원 배열 초기화
+            int[] arrint = { 10, 20, 30, 40, 50 };
+            for (int i = 0; i < arrint.Length; i++)
+            {
+                Console.Write(arrint[i] + " ");
+                string str = string.Format("1차원 배열[{0}]: {1}", i, arrint[i]);
+                Console.WriteLine(str);
+            }
+
+            // 1차원 배열 초기화 2
+            char[] arrCh = new char[4]
+            {
+                '박', '김','이','최'
+            };
+
+            for (int i = 0; i < arrCh.Length; i++)
+            {
+                Console.Write(arrCh[i] + " ");
+                string str = string.Format("1차원 배열[{0}]: {1}", i, arrCh[i]);
+                Console.WriteLine(str);
+
+            }
+
+            // 2차원 배열 초기화 1
+            int[,] arrint2 =
+            {
+                {1,2,3}, {4,5,6}
+            };
+            for (int i = 0; i< 2; i++)
+            {
+                for(int j=0; j<3;j++)
+                {
+                    Console.Write(arrint2[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Basic060921/Basic060921/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic060921
+{
+    class Program
+    {
+        /// <summary>
+        /// 메인함수
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            //출력
+            Console.WriteLine("안녕하세요");
+            Console.WriteLine("핼로우 C#");
+
+            //입력
+            Console.Write("이름 입력: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("이름: " + name);
+
+            Console.WriteLine("나이 입력: ");
+            //method 1
+            //string age = Console.ReadLine();
+            //int nAge = Convert.ToInt32(age);
+
+            //method 2
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("나이 : {0}", age);
+            Console.WriteLine("나이 : {0}, 이름: {1}", age,name);
+
+            //method 3
+            string str = string.Format("이름: {0}, 나이:{1}", name, age);
+            Console.WriteLine(str);
+        }
+    }
+}
